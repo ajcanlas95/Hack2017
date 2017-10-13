967cf4da0e271cb90fcce77e2b76fc7f89a671ce
bubuguduystunstugudunstuy <jomillematildo@ymail.com>
2017-10-13 21:50:25 +0800

    added capitalize and uppercase function to all inputs.

---
+++ website/backend/register.php
@@ -0,0 +1,46 @@
+<?php
+require 'connect.php';
+require 'function/user.function.php';
+require 'function/validate.input.php';
+if (isset($_POST['register'])) {
+	$fName = validate($_POST['first_name']);
+	$mName = validate($_POST['middle_name']);
+	$lName = validate($_POST['last_name']);
+	$nName = validate($_POST['nick_name']);
+	$pName = validate($_POST['post_name']);
+	$eMail = validate($_POST['email']);
+	$cNumber = validate($_POST['contact_number']); 
+	$orgName = validate($_POST['organization']); 
+	$preOrg = validate($_POST['organization_prefix']);
+	
+	$fName = ucwords($fName);
+	$mName = ucwords($mName);
+	$lName = ucwords($lName);
+	$pName = ucwords($pName);
+	$orgName = ucwords($orgName);
+	$preOrg = strtoupper($preOrg);
+	
+	$orgExists = checkOrganization($conn,$orgName);
+
+	if (mysqli_num_rows($orgExists)<1) {
+		$registerInfo = registerBasic($conn,$fName,$mName,$lName,$nName,$pName,$eMail,$cNumber);
+
+		if ($registerInfo) {
+			$registerOrg = registerOrganization($conn,$orgName,$preOrg);
+
+			if ($registerOrg) {
+				echo "Successful Registration";
+			}else{
+				echo "Org Registration Unsuccessful";
+			}
+
+		}else{
+			echo "Info Registration Unsuccessful";
+		}
+	}else{
+		echo "Organization Exists";
+	}
+
+}
+
+?>
