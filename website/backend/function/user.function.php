d4f7f13a352a0dd3dc650ceab34b379de7c8eba9
bubuguduystunstugudunstuy <jomillematildo@ymail.com>
2017-10-13 21:22:54 +0800

    Added register.php that can save basic information about the user including the organization he's into.
    Feature:
    1. Check if Org Exists.
    2. Validate all inputs from registration.html.

--- website/backend/function/user.function.php
+++ website/backend/function/user.function.php
@@ -9,4 +9,20 @@ function loginEmail($conn,$email,$organization,$password){
 
 	return mysqli_query($conn,$query);
 }
+function registerBasic($conn,$first_name,$middle_name,$last_name,$nick_name,$post_name,$email,$contact_number){
+	$query = "insert into basic (fn_name,mn_name,ln_name,nn_name,pof_name,email,contact_number) values('$first_name','$middle_name','$last_name','$nick_name','$post_name','$email','$contact_number')";
+
+	return mysqli_query($conn,$query);
+}
+function registerOrganization($conn,$organization,$organization_prefix){
+	$query = "insert into organization (organization_name,organization_table_prefix) values('$organization','$organization_prefix')";
+
+	return mysqli_query($conn,$query);
+}
+function checkOrganization($conn,$organization){
+	$query = "select organization_name from organization where organization_name = '$organization'";
+
+	return mysqli_query($conn,$query);
+}
+
 ?>
